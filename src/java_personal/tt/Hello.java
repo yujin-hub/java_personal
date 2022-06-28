7a7d270b45fc4f4deee84bbf1079fa267cc608ac
cuj0405@naver.com <cuj0405@naver.com>
2022-06-28 10:26:39 +0900

    .

---
+++ src/java_personal/tt/Hello.java
@@ -0,0 +1,11 @@
+package java_personal.tt;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD!!");
+		
+	}
+
+}
